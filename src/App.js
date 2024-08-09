cf0e967f88f654f1d61507061ac4b28685260386
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-08-09 04:41:46 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import "./index.css"
+import Home from "./routes/Home"
+import Contact from "./routes/Contact"
+import Project from "./routes/Project"
+import About from "./routes/About"
+import { Route } from 'react-router-dom';
+import {Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/project" element={<Project/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
