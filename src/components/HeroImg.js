249a249bc3a8b1c683096d15715ec3ff69f47f7e
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-08-10 19:41:12 +0300

    Add files via upload

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,11 +1,23 @@
 import "./HeroImgStyles.css"
 
 import React from 'react'
+import IntroImg from "../assets/doSomethingGreat.jpg"
+import { Link } from "react-router-dom"
 
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
+            <img className="into_img"
+                src={IntroImg} alt="ItroImg"/>
+        </div>
+        <div className="content">
+            <p>Hi I am a freelancer</p>
+            <h1>React Dev</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn-light">Contact</Link>
+            </div>
 
         </div>
     </div>
