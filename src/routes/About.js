f6bb9b07d3317411f9ef4cfaebd22d0ede3c56b9
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-08-11 15:56:31 +0300

    Add files via upload

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const About = () => {
-  return (
-    <div>
-      <Navbar/>
-      <Footer/>
-    </div>
-  )
-}
+const About = () => 
+  {
+    return (
+          <div>
+            <Navbar/>
+            <Footer/>
+          </div>
+    );
+  };
 
 export default About
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,7 +11,7 @@ const Home = () =>
             <HeroImg/>
             <Footer/>
         </div>
-    )
-}
+    );
+};
 
 export default Home
